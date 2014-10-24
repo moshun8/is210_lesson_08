034c75b57850427ea241542de05df42bc33e767e
Maureen Kilmade <maureen.kilmade@spsmail.cuny.edu>
2014-10-24 12:10:07 -0400

    task 4

---
+++ task_04.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Lesson 08, Task 04 file"""
+
+
+import task_02
+
+
+class Tigerpaw(task_02.Tire):
+    """The foot of a tiger.
+
+    Args:
+        
+    Attributes:
+       Overrides the _Tire__maximum_tires from task 2
+    """
+
+    _Tire__maximum_tires = 750
